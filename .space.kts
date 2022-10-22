2dd69572b6f36aac5aba233bb5b3c1fd91503220
Adrian Witaszak <adrianwitaszak@gmail.com>
2022-10-22 13:35:16 +0000

    pipeline added docker deploy
    removed gcp
    
    Merge-request: SHOPPE-MR-2
    Merged-by: Adrian Witaszak <adrianwitaszak@gmail.com>

--- .space.kts
+++ .space.kts
@@ -1,8 +1,10 @@
+import circlet.pipelines.script.SpaceApi
+
 /**
-* JetBrains Space Automation
-* This Kotlin-script file lets you automate build activities
-* For more info, see https://www.jetbrains.com/help/space/automation.html
-*/
+ * JetBrains Space Automation
+ * This Kotlin-script file lets you automate build activities
+ * For more info, see https://www.jetbrains.com/help/space/automation.html
+ */
 
 job("Build and run tests") {
     startOn {
@@ -12,16 +14,46 @@ job("Build and run tests") {
             }
         }
     }
+//
+//    container(displayName = "Gradle build", image = "gradle:jdk11") {
+//        kotlinScript { api ->
+//            api.space().doSafely("Build failed") {
+//                api.gradlew("clean")
+//                api.gradlew("build")
+//            }
+//            api.space().doSafely("Build failed") {
+//                api.gradlew("test")
+//            }
+//        }
+//    }
 
-    container(displayName = "Gradle build", image = "gradle:jdk11") {
-        kotlinScript { api ->
-            if (api.gitBranch() == "refs/heads/master"){
-                println("Running in master branch")
-            }
-            api.gradlew("clean")
-            api.gradlew("build")
-            api.gradlew("test")
-            api.gradlew("appengineDeploy")
+    host("Build artifacts and a Docker image") {
+        // assign project secrets to environment variables
+        env["HUB_USER"] = Secrets("dockerhub_user")
+        env["HUB_TOKEN"] = Secrets("dockerhub_token")
+
+        shellScript {
+            content = """
+                docker login --username ${'$'}HUB_USER --password "${'$'}HUB_TOKEN"
+            """
         }
+
+        dockerBuildPush {
+            labels["vendor"] = "mycompany"
+            tags {
+                +"shoppe-api/release:1.0.${"$"}JB_SPACE_EXECUTION_NUMBER"
+            }
+        }
+    }
+}
+
+
+suspend fun SpaceApi.doSafely(messageIfFailed: String, block: () -> Unit) {
+    try {
+        block()
+    } catch (ex: Exception) {
+        val recipient = ChannelIdentifier.Profile(ProfileIdentifier.Id("adrianwitaszak"))
+        val content = ChatMessage.Text(messageIfFailed)
+        chats.messages.sendMessage(recipient, content)
     }
 }
