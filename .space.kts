f78958bacda0c66860d15960bb2e8281b6febd76
Adrian Witaszak <adrianwitaszak@gmail.com>
2022-10-22 19:49:55 +0100

    local worker

--- .space.kts
+++ .space.kts
@@ -7,9 +7,14 @@ job("deploy") {
         }
     }
 
-    container(displayName = "Gradle build", image = "gradle:jdk11") {
+    requirements {
+        workerPool = WorkerPools.SELF_HOSTED
+    }
+
+    container(displayName = "Build, Test and GCP deploy", image = "gradle:jdk11") {
         kotlinScript { api ->
                 api.gradlew("build")
+                api.gradlew("test")
                 api.gradlew("appengineDeploy")
         }
     }
