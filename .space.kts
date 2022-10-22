f6fd994275a5696dfb5fc05321d1791273084886
Adrian Witaszak <adrianwitaszak@gmail.com>
2022-10-22 20:10:01 +0100

    local worker

--- .space.kts
+++ .space.kts
@@ -11,11 +11,32 @@ job("deploy") {
         workerPool = WorkerPools.SELF_HOSTED
     }
 
-    container(displayName = "Build, Test and GCP deploy", image = "gradle:jdk11") {
+    container(displayName = "Build and test", image = "gradle:jdk11") {
         kotlinScript { api ->
-                api.gradlew("build")
-                api.gradlew("test")
-                api.gradlew("appengineDeploy")
+            try {
+                api.gradle("build", "-x", "test")
+            } catch (ex: Exception) {
+                val recipient = ChannelIdentifier.Channel(ChatChannel.FromName("CI-channel"))
+                val content = ChatMessage.Text("Build failed")
+                api.space().chats.messages.sendMessage(recipient, content)
+            }
+        }
+    }
+
+    host(displayName = "GCP deploy") {
+        shellScript {
+            content = """
+                ./gradlew appengineDeploy
+            """
+        }
+    }
+
+    failOn {
+        testFailed { enabled = false }
+        nonZeroExitCode { enabled = false }
+        outOfMemory { enabled = false }
+        timeOut {
+            runningTimeOutInMinutes = 15
         }
     }
 }
