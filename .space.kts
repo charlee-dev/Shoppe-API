0382d333d2ae025cc8c74557ba4af799c62aa85c
Adrian Witaszak <adrianwitaszak@gmail.com>
2022-10-22 19:35:10 +0100

    removed try catch

--- .space.kts
+++ .space.kts
@@ -15,23 +15,10 @@ job("Build and run tests") {
 
     container(displayName = "Gradle build", image = "gradle:jdk11") {
         kotlinScript { api ->
-            api.space().doSafely("Build failed") {
                 api.gradlew("clean")
                 api.gradlew("build")
 //                api.gradlew("test")
                 api.gradlew("appengineDeploy")
-            }
         }
-    }
-}
-
-
-suspend fun circlet.pipelines.script.SpaceApi.doSafely(messageIfFailed: String, block: () -> Unit) {
-    try {
-        block()
-    } catch (ex: Exception) {
-        val recipient = ChannelIdentifier.Profile(ProfileIdentifier.Id("adrianwitaszak"))
-        val content = ChatMessage.Text(messageIfFailed)
-        chats.messages.sendMessage(recipient, content)
     }
 }
