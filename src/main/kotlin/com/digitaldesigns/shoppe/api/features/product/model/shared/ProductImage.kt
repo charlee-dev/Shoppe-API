57b0db72e0d56f2a883230e2817e1ae4046bda8b
Adrian Witaszak <adrianwitaszak@gmail.com>
2022-12-06 10:05:48 +0000

    updated graphql descriptions

--- src/main/kotlin/com/digitaldesigns/shoppe/api/features/product/model/shared/ProductImage.kt
+++ src/main/kotlin/com/digitaldesigns/shoppe/api/features/product/model/shared/ProductImage.kt
@@ -8,16 +8,10 @@ import com.expediagroup.graphql.generator.annotations.GraphQLDescription
 
 @GraphQLDescription(GraphQLDesc.Product.Image.model)
 data class ProductImage(
-    @GraphQLDescription(GraphQLDesc.Product.Image.id)
-    override val id: String = generateId(),
-    @GraphQLDescription(GraphQLDesc.Product.Image.created)
-    override val created: String = "",
-    @GraphQLDescription(GraphQLDesc.Product.Image.modified)
-    override var modified: String = "",
-    @GraphQLDescription(GraphQLDesc.Product.Image.url)
-    var url: String = "",
-    @GraphQLDescription(GraphQLDesc.Product.Image.name)
-    var name: String = "",
-    @GraphQLDescription(GraphQLDesc.Product.Image.altText)
-    var altText: String = "",
+    @GraphQLDescription(GraphQLDesc.Product.Image.id) override val id: String = generateId(),
+    @GraphQLDescription(GraphQLDesc.Product.Image.created) override val created: String = "",
+    @GraphQLDescription(GraphQLDesc.Product.Image.modified) override var modified: String = "",
+    @GraphQLDescription(GraphQLDesc.Product.Image.url) var url: String = "",
+    @GraphQLDescription(GraphQLDesc.Product.Image.name) var name: String = "",
+    @GraphQLDescription(GraphQLDesc.Product.Image.altText) var altText: String = "",
 ) : Model, TimeTrackable
